ae304d260ac4d38a1f391bc525bb56bd68d12acc
Muhammad Talha <talhariaz5425869@gmail.com>
2024-07-21 05:24:47 +0000

    Updated

--- cryptos/hexn.js
+++ cryptos/hexn.js
@@ -1,78 +1,49 @@
-const claim = async()=>{
-    fetch("https://api.hexn.cc/v1/kyc/marketing/farming/claim/", {
-  "headers": {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8",
-    "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJIZXhuIiwic3ViIjoiQWNjZXNzIHRva2VuIiwiYXVkIjoiaGV4bl9zZXJ2aWNlc19wcm9kIiwiaWF0IjoxNzIxMzgxMzAyLCJleHAiOjE3MjE0MDI5MDIsInVpZCI6IkJRNVVlOEs5bGlYMW1WRHlwcWV4TGFoYWZlWmUiLCJjbGFpbXMiOnsicGxhdGZvcm0iOiJURUxFR1JBTSIsIm1mYSI6ZmFsc2UsInJ0aWQiOiIwNjRhOGQ4OC0yZGM3LTQ4ZjktOGZmYy03OGIwZDU1MzRlOGUifSwidWlkX2FmZmlsaWF0ZSI6IkJRNVVlOEs5bGlYMW1WRHlwcWV4TGFoYWZlWmUifQ.gz-FrJV5mW2Mi7dd3Tv_sOjKQqIgTu4czyS-7Xlb9RV6QscmZIcVMivwhRYNKfz1U4v4MpcanTue5d0UBqzsbr6o0YfhSWOsdirZyvdT9klg54Fc7tbUtwWWPUf6B2br3hLjkI2hmBE-dMWObxoeR__sHuyBzUFs-vCmCJlbXwfZV4shaIRMyVMlI2xBrsmQ_HEXROG1CljtTOrVMBbx3Cj2gerUXcZ4EHQ_N5obmyTAD4m90vy-_oyChQTwQIDQ47pCmgHgbiNASMbQ9hELH52BIv2BIQYD03izo9JxOIxhh8stx8k3_eZBV48ReYRVSnhUQuWe7CFciMKvLFfemA",
-    "content-type": "application/json",
-    "customer_uid": "BQ5Ue8K9liX1mVDypqexLahafeZe",
-    "fingerprint": "2e346efb3083b14470965fb9d9ccb95b",
-    "platform": "WEB",
-    "platform-version": "0.0.34",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "trace-uuid": "a0c63c50-ce3e-46b7-9196-02764df59f0a"
-  },
-  "referrerPolicy": "no-referrer",
-  "body": "{\"platform\":\"WEB\",\"farming_uuid\":\"6934a043-62e2-486f-ae73-e8ee6ed5651b\"}",
-  "method": "POST"
-}).then(res=>res.json()).then(res=>console.log(res)).catch(err=>console.log(err))
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer");
+const {setTimeout} = require('node:timers/promises');
+
+const urls = [
+  `https://tgapp.hexn.cc/#tgWebAppData=user%3D%257B%2522id%2522%253A6345740789%252C%2522first_name%2522%253A%2522%25D8%25B7%25D9%2584%25D8%25AD%25DB%2581%2522%252C%2522last_name%2522%253A%2522%2522%252C%2522username%2522%253A%2522TalhaRaiz%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26chat_instance%3D-2184345488286448641%26chat_type%3Dchannel%26auth_date%3D1721381164%26hash%3D98e62332a40b64e6a4dca1039716c876d9125dbc59d220b6eb32bf536808dc10&tgWebAppVersion=7.6&tgWebAppPlatform=android&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22secondary_bg_color%22%3A%22%23f0f0f0%22%2C%22text_color%22%3A%22%23222222%22%2C%22hint_color%22%3A%22%23a8a8a8%22%2C%22link_color%22%3A%22%232678b6%22%2C%22button_color%22%3A%22%2350a8eb%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22header_bg_color%22%3A%22%23527da3%22%2C%22accent_text_color%22%3A%22%231c93e3%22%2C%22section_header_text_color%22%3A%22%233a95d5%22%2C%22subtitle_text_color%22%3A%22%2382868a%22%2C%22destructive_text_color%22%3A%22%23cc2929%22%2C%22section_separator_color%22%3A%22%23d9d9d9%22%7D `
+];
+let browser = null;
+const LaunchBrowser = async()=>{
+  try {
+      browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error Launching Browser')
+  }
 }
+const func = async(url) => {
+  // Launch the browser
+  if(!browser) await LaunchBrowser()
+  // Open a new page
+  const page = await browser.newPage();
+  // Navigate to the desired URL
+  await page.goto(url);
+  // Wait for 10 seconds
+  await setTimeout(1000);
+  // Take a screenshot and save it
 
-const axios = require('axios');
+  setInterval(async ()=>{
+          try {
+            await page.screenshot({ path: './images/hexn.png' });
+            await page.click('.FarmingActions_farmingButton__3qY4A')
+           
+          } catch (error) {
+            console.log(error)
+          }
+      },10000)
 
+  // Close the browser
 
-
-const startFarming = ()=>{
-    fetch("https://api.hexn.cc/v1/kyc/marketing/farming/start/", {
-  "headers": {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8",
-    "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJIZXhuIiwic3ViIjoiQWNjZXNzIHRva2VuIiwiYXVkIjoiaGV4bl9zZXJ2aWNlc19wcm9kIiwiaWF0IjoxNzIxMzgxMzAyLCJleHAiOjE3MjE0MDI5MDIsInVpZCI6IkJRNVVlOEs5bGlYMW1WRHlwcWV4TGFoYWZlWmUiLCJjbGFpbXMiOnsicGxhdGZvcm0iOiJURUxFR1JBTSIsIm1mYSI6ZmFsc2UsInJ0aWQiOiIwNjRhOGQ4OC0yZGM3LTQ4ZjktOGZmYy03OGIwZDU1MzRlOGUifSwidWlkX2FmZmlsaWF0ZSI6IkJRNVVlOEs5bGlYMW1WRHlwcWV4TGFoYWZlWmUifQ.gz-FrJV5mW2Mi7dd3Tv_sOjKQqIgTu4czyS-7Xlb9RV6QscmZIcVMivwhRYNKfz1U4v4MpcanTue5d0UBqzsbr6o0YfhSWOsdirZyvdT9klg54Fc7tbUtwWWPUf6B2br3hLjkI2hmBE-dMWObxoeR__sHuyBzUFs-vCmCJlbXwfZV4shaIRMyVMlI2xBrsmQ_HEXROG1CljtTOrVMBbx3Cj2gerUXcZ4EHQ_N5obmyTAD4m90vy-_oyChQTwQIDQ47pCmgHgbiNASMbQ9hELH52BIv2BIQYD03izo9JxOIxhh8stx8k3_eZBV48ReYRVSnhUQuWe7CFciMKvLFfemA",
-    "content-type": "application/json",
-    "customer_uid": "BQ5Ue8K9liX1mVDypqexLahafeZe",
-    "fingerprint": "2e346efb3083b14470965fb9d9ccb95b",
-    "platform": "WEB",
-    "platform-version": "0.0.34",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "trace-uuid": "c1d5f36a-e340-4fd9-84ce-6ec45408e7f5"
-  },
-  "referrerPolicy": "no-referrer",
-  "body": "{\"platform\":\"WEB\"}",
-  "method": "POST"
-}).then(res=>res.json()).then(res=>console.log(res)).catch(err=>console.log(err))
-}
-
-const token = async()=>{
-  fetch("https://api.hexn.cc/v1/kyc/auth/refresh-token", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8",
-    "priority": "u=1, i",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site"
-  },
-  "referrerPolicy": "no-referrer",
-  "body": null,
-  "method": "OPTIONS"
-});
-}
+};
 
 
-setInterval(()=>{
-    claim()
-    startFarming()
-    
-},60000)
+ func(urls[0])
+
